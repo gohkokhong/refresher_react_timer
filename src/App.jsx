8a3b2b76a8a64401441b390cda9b2faf443023f8
Goh Kok Hong <kokhong.goh@gmail.com>
2025-08-27 13:26:02 +0800

    Replace input example with timer using useRef and useState
    Timer with start stop and reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,50 @@
-import { useRef } from "react";
+import { useState, useRef } from "react";
 
 const App = () => {
-  const inputRef = useRef(null);
-  const submit = () => {
-    console.log(inputRef.current);
-    console.log(inputRef.current.value);
-    inputRef.current.style.background = 'red';
-    inputRef.current.style.color = 'white';
-    inputRef.current.setAttribute('placeholder', 'Updated');
+  const timerRef = useRef(null);
+
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const toggleTimer = () => {
+    if (isRunning) {
+      // Clear interval to stop the timer
+      clearInterval(timerRef.current);
+      timerRef.current = null;
+    } else {
+      // Start timer
+      timerRef.current = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000)
+    }
+
+    setIsRunning(!isRunning);
+  };
+
+  const resetTimer = () => {
+    clearInterval(timerRef.current);
+    setIsRunning(false);
+    setTime(0);
+    timerRef.current = null;
   }
 
   return (
     <div className="max-w-md mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg text-center">
-      <h2 className="text-2xl font-bold mb-4">
-        useRef Example
-      </h2>
+      <h2 className="text-4xl font-semibold mt-4">⏳ Timer: {time}</h2>
+      <button 
+        className="mt-3 bg-green-500 text-white px-4 py-2 mr-3 rounded hover: bg-green-600"
+        onClick={toggleTimer}
+      >
+          { isRunning ? 'Pause' : 'Start'}
+      </button>
 
-      <input 
-        type="text" 
-        placeholder="Type something..."
-        className="w-full p-2 border rounded-lg"
-        ref={inputRef}
-      />
-
-      <button onClick={submit} className="mt-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-        Submit
+      <button 
+        className="mt-3 bg-red-500 text-white px-4 py-2 rounded hover: bg-green-600"
+        onClick={resetTimer}
+      >
+          Reset
       </button>
     </div>
-   );
+  )
 }
- 
+
 export default App;
